59417bba8470d3aeb9998fa9d6ebfe766ef8203b
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2022-12-29 17:04:45 +0100

    TODO add eventHandler!!

--- charts/pieChart.js
+++ charts/pieChart.js
@@ -55,7 +55,7 @@ export function pieChart(obj) {
                 startAngle: slices[ i - 1 ]?.startAngle + slices[ i - 1 ]?.sweepAngle || 0,
                 percent: `${Math.round(degPercent * sortedValues[ i ] / 3.6)}%`,
                 stroke: obj.colors[ i ] || randomHex(),
-                opacity: 1,
+                value: sortedValues[i]
             }
 
         )
@@ -85,7 +85,7 @@ export function pieChart(obj) {
         const a = s.startAngle;
         const o = a + s.sweepAngle;
         const start = getCoords(cx, cy, _radius, a);
-        
+        const charCoords = getCoords(cx, cy, _radius, a + s.sweepAngle / 2)
         const end = getCoords(cx, cy, _radius, o);
         const dir = s.sweepAngle > 0 ? 1 : 0;
         const swap = Math.abs(s.sweepAngle) % 360 < 180 ? 0 : 1;
@@ -101,14 +101,14 @@ export function pieChart(obj) {
             'type': 'path',
             'options': {
                 id: `slice${pieCount}_${sliceCount}`,
-                class: "slice popup",
+                class: "slice tooltip",
                 d: d,
                 stroke: s.stroke,
                 strokeWidth: strokeWidth,
 
             }
         };
-        function createRing() {
+        function createInfo() {
             let ringR = obj.r + 8;
             let start = getCoords(cx, cy, ringR, a);
             let end = getCoords(cx, cy, ringR, o);
@@ -132,12 +132,30 @@ export function pieChart(obj) {
         
                 }
             };
+            
             const newRing = createSVG(ring.type, ring.options);
             newPie.appendChild(newRing);
-            
-        }
-        function removeRing() {
+            const toolTipString =
+                `<svg  id="toolTip${pieCount}_${sliceCount}" width="150px" height="100px" x ="${charCoords.x -75}px" y="${charCoords.y-40}px">
+                    <rect width="100%" height="100%" fill="black" opacity="0.5" rx="5px" ry="5px"></rect>
+                    <text class="tooltiptext" ` +
+                    `x ="50%" y="50%" `+
+                    `font-size="22" text-anchor="middle" fill = "white" dominant-baseline="central">
+                    <tspan x="50%" y="30px">DATA</tspan>
+                    <tspan x="50%" y="60px">${s.value}</tspan>
+                    </text >
+                 </svg >`
+            // CREATE DOM PERCENTAGE
+            let newToolTip = parser.parseFromString(toolTipString, 'text/html').body.childNodes[ 0 ];
+            newPie.appendChild(newToolTip);
+
+        };
+        
+        
+        function removeInfo() {
             document.getElementById(`ring${pieCount}_${sliceCount}`)?.remove();
+            document.getElementById(`toolTip${pieCount}_${sliceCount}`)?.remove();
+            
         }
 
         // CREATE DOM ELS
@@ -149,11 +167,15 @@ export function pieChart(obj) {
 
         // PERCENTAGE 
         if (obj.percentage) {
-            const charCoords = getCoords(cx, cy, _radius, a + s.sweepAngle / 2)
+           
             let percString =
                 // TODO why does this need to be wrapped in an additional svg in order to get rendered???
                 // and text doesn't get applied if created different than using parser????
-                `<svg><text id="perc${pieCount}_${sliceCount}" class="perc" x="${charCoords.x}" y="${charCoords.y}"  font-size="${obj.fontSize}px" font-weight="bolder" text-anchor="middle" alignment-baseline="central" fill="${obj.color}" opacity="1" text-content="${s.percent}">${s.percent}</text></svg>`
+                `<svg>
+                    <text id="perc${pieCount}_${sliceCount}" class="perc" ` +
+                    `x="${charCoords.x}" y="${charCoords.y}" `+
+                `font-size="${obj.fontSize}px"  fill="${obj.color}" dominant-baseline="central">${s.percent}</text>
+                </svg>`
 
             // CREATE DOM PERCENTAGE
             const newPerc = parser.parseFromString(percString, 'text/html').body.childNodes[ 0 ];
@@ -162,31 +184,46 @@ export function pieChart(obj) {
 
         };
         let perc = document.getElementById(`perc${pieCount}_${sliceCount}`);
-        let clicks = 0;
+       
         let shownData = [ sortedValues[ sort ], s.percent, ]
         let slice = document.getElementById(`slice${pieCount}_${sliceCount}`);
+        console.log(slice)
         
-        
-        // CREATE on click???
-        function myFunction() {
-            var popup = document.getElementById("myPopup");
-            popup.classList.toggle("show");
-        }
+        // // CREATE on click???
+        // function myFunction() {
+        //     var popup = document.getElementById("myPopup");
+        //     popup.classList.toggle("show");
+        // }
      
         // TODO show data as popup and mark selected instead
-        slice.addEventListener('click', () => {
-            myFunction()
-            clicks %= 2
-            perc.textContent = shownData[ clicks ];
-            clicks === 0 ? createRing() : removeRing();
+        
+        let clicks = 0;
+        let isClicked = undefined;
+        slice.addEventListener('click', (e) => {
+           isClicked = document.getElementById(e.target.id)
+            console.log(isClicked)
+            if (clicks % 2 === 0) {
+                perc.style.opacity = 0;
+                createInfo(); 
+            } else {
+                perc.style.opacity = 1;
+                removeInfo();
+            } 
             clicks++;
+           
+            console.log(`clicks%2: ${clicks % 2}`)
+            isClicked?.addEventListener('mouseleave', () => {
+
+                removeInfo();
+                perc.style.opacity = 1
+                console.log(`clicks: ${clicks}`);
+                clicks = 0;
+            });
+           
         });
-        //TODO remove ring
-        slice.addEventListener('mouseleave', () => {
-            removeRing();
-            perc.textContent = shownData[ 1 ];
-            clicks = 0;
-        });
+
+       
+        
         sliceCount++;
     });
 
